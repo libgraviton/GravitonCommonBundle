310425fbc1c2a9f1d49559d4c7777aab44422d8a
Dario Nuevo <dn@xbe.ch>
2021-08-18 15:46:31 +0200

    unused imports..

--- src/Component/Deployment/VersionInformation.php
+++ src/Component/Deployment/VersionInformation.php
@@ -7,12 +7,6 @@ namespace Graviton\CommonBundle\Component\Deployment;
 
 use Composer\InstalledVersions;
 use Doctrine\ODM\MongoDB\DocumentManager;
-use Graviton\CommonBundle\Document\Deployment;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * @author  List of contributors <https://github.com/libgraviton/DeploymentServiceBundle/graphs/contributors>
